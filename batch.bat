ca06a8accf42be95efa63231aee94c45bf4af608
niruk2004 <niranjank2929@gmail.com>
2025-01-10 00:13:42 +0530

    stage15

--- batch.bat
+++ batch.bat
@@ -6,10 +6,12 @@ load --exhandler $HOME/myexpos/spl/spl_progs/haltprog.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/timer_stage14.xsm
 
 
-load --int=7 $HOME/myexpos/spl/spl_progs/sample_int7.xsm
+load --int=7 $HOME/myexpos/spl/spl_progs/int7_stage15.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int_10_stage14.xsm
 
 load --init $HOME/myexpos/expl/expl_progs/oddfrom1to100.xsm
 
+load --module 0 $HOME/myexpos/spl/spl_progs/module0_stage15.xsm
+load --module 4 $HOME/myexpos/spl/spl_progs/module4_stage15.xsm
 load --module 5 $HOME/myexpos/spl/spl_progs/module_5_stage14.xsm
-load --module 7 $HOME/myexpos/spl/spl_progs/boot_mod_stage14.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/boot_mod_stage15.xsm
