1a15c5a4853efe9d0b5481ef636697f022ca63b8
niruk2004 <niranjank2929@gmail.com>
2025-01-10 21:26:58 +0530

    stage16

--- batch.bat
+++ batch.bat
@@ -4,14 +4,15 @@ load --idle $HOME/myexpos/expl/expl_progs/idle_process.xsm
 
 load --exhandler $HOME/myexpos/spl/spl_progs/haltprog.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/timer_stage14.xsm
+load --int=console $HOME/myexpos/spl/spl_progs/console_int_stage16.xsm
 
-
+load --int=6 $HOME/myexpos/spl/spl_progs/int6_stage16.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/int7_stage15.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int_10_stage14.xsm
 
 load --init $HOME/myexpos/expl/expl_progs/oddfrom1to100.xsm
 
 load --module 0 $HOME/myexpos/spl/spl_progs/module0_stage15.xsm
-load --module 4 $HOME/myexpos/spl/spl_progs/module4_stage15.xsm
+load --module 4 $HOME/myexpos/spl/spl_progs/module4_stage16.xsm
 load --module 5 $HOME/myexpos/spl/spl_progs/module_5_stage14.xsm
-load --module 7 $HOME/myexpos/spl/spl_progs/boot_mod_stage15.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/boot_mod_stage16.xsm
