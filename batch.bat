c5b59746b1bdbcfd158af19e4df6afc1cc02b143
niruk2004 <niranjank2929@gmail.com>
2025-01-12 22:43:00 +0530

    stage17

--- batch.bat
+++ batch.bat
@@ -8,11 +8,14 @@ load --int=console $HOME/myexpos/spl/spl_progs/console_int_stage16.xsm
 
 load --int=6 $HOME/myexpos/spl/spl_progs/int6_stage16.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/int7_stage15.xsm
+load --int=9 $HOME/myexpos/spl/spl_progs/int9_stage17.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int_10_stage14.xsm
 
-load --init $HOME/myexpos/expl/expl_progs/oddfrom1to100.xsm
+load --init $HOME/myexpos/expl/expl_progs/st17q1.xsm
 
 load --module 0 $HOME/myexpos/spl/spl_progs/module0_stage15.xsm
+load --module 1 $HOME/myexpos/spl/spl_progs/module1_stage17.xsm
+load --module 2 $HOME/myexpos/spl/spl_progs/module2_stage17.xsm
 load --module 4 $HOME/myexpos/spl/spl_progs/module4_stage16.xsm
 load --module 5 $HOME/myexpos/spl/spl_progs/module_5_stage14.xsm
-load --module 7 $HOME/myexpos/spl/spl_progs/boot_mod_stage16.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/boot_mod_stage17.xsm
